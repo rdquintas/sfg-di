6d8781336474c8cb9fef28b370ccbc2033424372
Ricardo Quintas <rdquintas@yahoo.com>
2021-05-22 22:57:45 +0100

    initial build

---
+++ src/test/java/zrq/com/sfgdi/SfgDiApplicationTests.java
@@ -0,0 +1,13 @@
+package zrq.com.sfgdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
